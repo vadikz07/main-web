eabc897af9fb871a76f8c9d45d10c89d63f95b74
vadik <vadikz@gmail.com>
2023-10-01 21:11:54 +0200

    Updated dice behaviour

--- src/pages/utils/DiceRoll.jsx
+++ src/pages/utils/DiceRoll.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Dice(props){
-    let num = 0
-    function genRandomNum(){
-        num = Math.random()*parseInt(props.sides)+1;
-        num = Math.floor(num)
-        return num
-    }
-    
-    return <div>
-        rolling die of {props.sides} , result {genRandomNum()}
+function Dice(props) {
+  const [roll, setRoll] = useState("Pulsa Roll");
+  const [sides, setSides] = useState(6);
+
+  function getRandomNum(sides) {
+    var num = Math.floor(Math.random() * sides + 1);
+    setRoll(num);
+    return num;
+  }
+
+  function increaseSides() {
+    setSides(sides + 1);
+  }
+
+  function decreaseSides() {
+    setSides(sides - 1);
+  }
+
+  return (
+    <div>
+      <div>
+        Roll: {roll} // Sides: {sides}
+      </div>
+      <div>
+        <button onClick={() => getRandomNum(sides)}>Roll!</button>
+      </div>
+      <div>
+        <button onClick={increaseSides}>Increase</button>
+        <button onClick={decreaseSides}>Decrease</button>
+      </div>
     </div>
+  );
 }
 
-export default Dice
+export default Dice;
