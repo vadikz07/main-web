b402d8788241db819b497ed4aca47ff6ff1c5adb
vadik <vadikz@gmail.com>
2023-10-11 16:41:53 +0200

    Sticky notes showing correctly now

--- src/components/StickyNote.jsx
+++ src/components/StickyNote.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+// props of StickyNote
+// text
+
 function StickyNote(props) {
   return (
     <div className="sticky-note-group">
       <div className="sticky-check-text-div">
         <button className="btn-check-sticky btn-sticky"/>
-        <div className="sticky-text">This is the text</div>
+        <div className="sticky-text">{props.text}</div>
       </div>
        <button className="btn-remove-sticky btn-sticky"></button>
     </div>
--- src/pages/utils/StickyNotes.jsx
+++ src/pages/utils/StickyNotes.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import StickyNote from "../../components/StickyNote";
 
 function StickyNotes() {
-  function populateStickyNotes() {
-    const result = [];
-    for (let i = 0; i < 3; i++) {
-      result.push(<StickyNote />);
-    }
-    return result;
+  //   const notes = [<StickyNote text="1" />, <StickyNote text="2" />]; //Lista de objetos sticky
+  const [notes, setNotes] = useState([]);
+
+  function generateSticky(valuetext) {
+    var newItem = <StickyNote key={notes.length} text={valuetext} />;
+    setNotes((prevNotes) => {
+      const newNotes = [...prevNotes, newItem];
+      console.log(newNotes);
+      return newNotes;
+    });
   }
 
   return (
     <div className="sticky-container shadow-box">
       <div className="sticky-input-container">
-        <form>
-          <input type="text" placeholder="Enter a note here." />
-          <input type="submit" value="Add" />
-        </form>
+        <div className="form-add-sticky">
+          <input id="form-text" type="text" placeholder="Enter a note here." />
+          <button
+            onClick={() =>
+              generateSticky(document.querySelector("#form-text").value)
+            }
+            type="submit"
+            value="Add"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      
-      <div className="sticky-notes-container">{populateStickyNotes()}</div>
+
+      <div className="sticky-notes-container">{notes.map((elem) => elem)}</div>
     </div>
   );
 }
